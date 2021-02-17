d6fa40abd20c1c65338cbb5a0dc4490072bad057
siddheshshinde07 <shindesiddhesh07@gmail.com>
2021-02-17 18:42:40 +0530

    uploading python task

---
+++ Personel/Siddhesh/Python/17Feb/SquareRoot.py
@@ -0,0 +1,5 @@
+
+#program to find Square root of number.
+Number = int(input("Enter the no : "))
+sqrt = Number ** 0.5
+print("square root of the number is = ", sqrt)
---
+++ Personel/Siddhesh/Python/17Feb/Swap.py
@@ -0,0 +1,11 @@
+#program to swap two variables 
+num1 = 10
+num2 = 20
+
+print("before swap = ",num1, num2)
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("after swap = ",num1, num2)
