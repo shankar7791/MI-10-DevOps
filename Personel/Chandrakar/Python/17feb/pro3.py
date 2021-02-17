8cf5d581bf2616313b4be7c5d7fd993d87e23c98
chandu1405 <chandrakarbhagat222@gmail.com>
2021-02-17 15:50:35 +0530

    uploading Python file

---
+++ Personel/Chandrakar/Python/17feb/pro3.py
@@ -0,0 +1,6 @@
+a = int (input("Enter 1st side: "))
+b = int (input("Enter 2nd side: "))
+c = int (input("Enter 3rd side: "))
+s = (a+b+c)/2
+area =(s*(s-a)*(s-b)*(s-c))**0.5
+print("triangle area is = " ,area)
---
+++ Personel/Chandrakar/Python/17feb/pro5.py
@@ -0,0 +1,2 @@
+import random
+print("Generated Random Number:",random.randint(0,100))
