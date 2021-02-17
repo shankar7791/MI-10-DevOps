0c228f258fcc8e0585b84e9f7d02d559353cd62c
kavi1410 <elankavi432@hmail.com>
2021-02-17 15:54:04 +0530

    uploading python task feb17 into Personal/Elankavi

---
+++ Personel/Elankavi/python/feb17/Add two numbers .py
@@ -0,0 +1,4 @@
+a = int(input("enter your first number : "))
+b = int(input ("enter your secound number : "))
+c = a+b
+print("The Addition of two number is  : " , c)
---
+++ Personel/Elankavi/python/feb17/celcious to fahrenheit.py
@@ -0,0 +1,3 @@
+c = int(input("Enter the Temp in celsius : "))
+f= (c * 1.8)+32
+print("Temp in fahrenheit is : ", f)
