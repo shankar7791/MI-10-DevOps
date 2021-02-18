194072ebe111af491b505e1f101b6f0115299875
sandeshpatekar <sandeshpatekar20@gmail.com>
2021-02-18 12:54:13 +0530

    uploading .py files

---
+++ Personel/Sandesh/Python/Practice/18Feb/if_statement.py
@@ -0,0 +1,3 @@
+num = int(input("enter the Number : "))
+if num > 100 :
+    print(num, "is greater than 100")
---
+++ Personel/Sandesh/Python/Practice/18Feb/nested_if.py
@@ -0,0 +1,19 @@
+num = int(input("enter the num : "))
+if num <=100:
+    if num <50 : 
+        print("num is less than 50")
+    else :
+        print("num is greater than 50")
+else :
+        if num > 75 :
+            if num >100 : 
+                print("i am grater than 100")
+            else :
+                print("i am less than 100")
+        else : 
+            print("i am greater than 75")
+
+       
+    
+    
+
