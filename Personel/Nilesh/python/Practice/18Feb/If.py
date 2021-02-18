392f8b2e50d83cad15484ce92e86949f7b443ea5
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-02-18 12:02:39 +0530

    uploading practice activity Nilesh/python/practice/18feb

---
+++ Personel/Nilesh/python/Practice/18Feb/If.py
@@ -0,0 +1,4 @@
+Number = int(input("enter the number : "))
+
+if Number >= 50 :
+    print("Right Number Entered")
---
+++ Personel/Nilesh/python/Practice/18Feb/if_else.py
@@ -0,0 +1,6 @@
+Num = int(input("enter the number to check even or odd : "))
+
+if Num % 2 == 0 :
+    print(f"{Num} is even")
+else:
+    print(f"{Num} is Odd")
