ac170a93e5817e4d5c766071c730732b5b4e265e
poojavarale137 <poojavarale137@gmail.com>
2021-02-18 11:39:01 +0530

    uploading 18feb into personel/pooja/python/practice

---
+++ Personel/pooja/python/practice/18feb/ifelse.py
@@ -0,0 +1,14 @@
+# if......else 
+#1.if statement
+#2.if ..else statement
+#3.nested if statement
+#4.if...elif...else ladder
+#5.shorhand if
+#6.shorthand if....else
+
+
+num  = int(input("enter a number:"))
+if num % 2 == 0:
+    print(f"{num} ,is even") 
+else: 
+    print(f"{num} ,is odd")  
---
+++ Personel/pooja/python/practice/18feb/nestedif.py
@@ -0,0 +1,24 @@
+# if......else 
+#1.if statement
+#2.if ..else statement
+#3.nested if statement
+#4.if...elif...else ladder
+#5.shorhand if
+#6.shorthand if....else
+
+num = int(input("Enter number : "))
+if num <= 50 :
+    if num < 25 : 
+         print("num is less than 25")
+    else :
+        print("number is greter than 25")
+else :
+    if num > 50 :
+        if num > 75 :
+            print("i am greter than 75")
+        else :
+            print(" i am less than 75")
+    else:
+        print("i am greter than 50")
+        
+    
