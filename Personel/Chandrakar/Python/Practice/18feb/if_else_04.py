8a9a44a03ce8168206975e27ef554e0869a53343
chandu1405 <chandrakarbhagat222@gmail.com>
2021-02-18 12:33:34 +0530

    uploading Python file^C

---
+++ Personel/Chandrakar/Python/Practice/18feb/if_else_04.py
@@ -0,0 +1,14 @@
+# if elif programming 
+a = int(input("enter the number : "))
+b = int(input("enter the 2nd nymber : "))
+c = int(input("enter the 2nd nymber : "))
+d = int(input("enter the 2nd nymber : "))
+if a < b :
+    print(f"{a} is  less {b} ")
+    
+elif b < c :
+     print(f"{b} is  less {c} ")
+elif c < d  :
+     print(f"{c} is  less {d} ")
+else :
+    print (" {a} greter than  b , c ,  d")         
---
+++ Personel/Chandrakar/Python/Practice/18feb/sho_ifelse_01.py
@@ -0,0 +1,3 @@
+a = 2
+b = 23
+print("A") if a > b else print("B")
