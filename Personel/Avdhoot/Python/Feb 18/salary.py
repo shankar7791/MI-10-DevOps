b29a7e33e97fffeebd08d4f596ea6ff0899716b9
avdhoot13 <64454908+avdhoot13@users.noreply.github.com>
2021-02-18 16:00:57 +0530

    Update salary.py

--- Personel/Avdhoot/Python/Feb 18/salary.py
+++ Personel/Avdhoot/Python/Feb 18/salary.py
@@ -1,20 +1,20 @@
 a = int(input("Enter Basic Salary :"))
 
 if a <= 10000 :
-    DA = (a * 40) / 100
+    DA = (a * 80) / 100
     HRA = (a * 20) / 100
     Gross_Salary = a + DA + HRA
     print("Gross Salary ", Gross_Salary)
 
 elif a >= 10001 and a <= 20000 :
-    DA = (a * 40) / 100
-    HRA = (a * 20) / 100
+    DA = (a * 90) / 100
+    HRA = (a * 25) / 100
     Gross_Salary = a + DA + HRA
     print("Gross Salary ", Gross_Salary)
 
 elif a >= 20001 :
-    DA = (a * 40) / 100
-    HRA = (a * 20) / 100
+    DA = (a * 95) / 100
+    HRA = (a * 30) / 100
     Gross_Salary = a + DA + HRA
     print("Gross Salary ", Gross_Salary)
 
