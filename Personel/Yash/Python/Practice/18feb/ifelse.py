4a823551b20a68eef89c040cdbd14cf5691f317c
coolyasu <malavade47@gmail.com>
2021-02-18 12:14:57 +0530

    Uploading practice  programs into Personel/Yash/Python/Practice

---
+++ Personel/Yash/Python/Practice/18feb/ifelse.py
@@ -0,0 +1,10 @@
+#if...else
+
+age=int(input("Enter the age : "))
+if age>=18 :
+    print(f"Your age is {age}")
+    print("You are eligible for voting")
+else :
+     print(f"Your age is {age}")
+     print("You are not eligible for voting") 
+
