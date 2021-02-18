1978a45f9a206df4bb490f00171c93798ffb2156
siddheshshinde07 <shindesiddhesh07@gmail.com>
2021-02-18 15:48:25 +0530

    uploading pythonstatement code

---
+++ Personel/Siddhesh/Practice/18feb/Ifstatement.py
@@ -0,0 +1,10 @@
+#if statemeent program 
+num = int(input("Enter the number "))
+if num%2==0:
+    print("number is even")
+
+"""
+num = int(input("Enter the number "))
+if num%2!=0:
+    print("number is Odd)
+"""
---
+++ Personel/Siddhesh/Practice/18feb/shorthandIf.py
@@ -0,0 +1,3 @@
+if 50==50: print("both are equal")
+
+if 50%2==0: print("this number is even")
