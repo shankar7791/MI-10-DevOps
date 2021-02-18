92b56ee418d85f821cee0b0b893b43a32c3a7cf2
harshlmeshram <hmeshram557@gmail.com>
2021-02-18 15:57:36 +0530

    uploading Python Files into Harshalm

---
+++ Personel/Harshalm/Python/18Feb/Prog4.py
@@ -0,0 +1,9 @@
+#divisible by 5 and 11 or not 
+
+num = int(input("Enter the number :"))
+
+if num % 5 == 0 and num % 11 == 0 :
+    print("Number is divisible by 5 and 11 ")
+else :
+    print("Number is not divisible by 5 and 11")
+    
