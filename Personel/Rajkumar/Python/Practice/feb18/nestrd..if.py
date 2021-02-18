046a9ca6cb20485cf31fce6e1f62b0dff9a5fe91
varmarajkumar745 <varmarajkumar745@gmail.com>
2021-02-18 12:49:26 +0530

    uploding practice program in to MI-10-DevOps/Personal/Rajkumar/Python/Practice/feb18

---
+++ Personel/Rajkumar/Python/Practice/feb18/nestrd..if.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a Value"))
+if a<=100:
+    if a<=50:
+        print("value less than 50")
+    else:
+        print("value grather than 50")
+else:
+    print("value grather than 100")
+    
