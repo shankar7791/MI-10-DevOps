b81fac9450e78dce57b040c347e2c366fa3879e5
akkig.shelkar@gmail.com <akkig.shelkar@gmail.com>
2021-02-21 16:52:52 +0530

    uploading 18Feb python tasks

---
+++ Personel/Akshay/Python/18Feb/1pos.py
@@ -0,0 +1,12 @@
+
+num=int(input("Enter the number"))
+
+if num<0 :
+    print(f"{num} Number is negitive")
+
+if num>0 :
+    print(f"{num} Number is positive")
+
+if num==0 :
+    print(f"{num} Number is Zero")
+
