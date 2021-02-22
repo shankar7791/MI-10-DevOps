6266f75be41e57639ebb76d5650f9817966019c3
varmarajkumar745 <varmarajkumar745@gmail.com>
2021-02-22 17:56:26 +0530

    uplodung a Quation and Answer

--- Personel/Rajkumar/Python/Practice/feb18/if..else.py
+++ Personel/Rajkumar/Python/Practice/feb18/if..else.py
@@ -1,5 +1,7 @@
-d=int(input("Enter a number: ")) 
-if d<=50:
-    print(f"number {d} less than and equal to 50")
-else :
-    print(f"number {d} grether than 5o")
+#d=int(input("Enter a number: ")) 
+#if d<=50:
+#    print(f"number {d} less than and equal to 50")
+#else :
+ #   print(f"number {d} grether than 5o")
+a=1
+print(datatype())
