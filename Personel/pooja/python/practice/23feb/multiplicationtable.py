648b4df85f9272a3482b7b983f287e068873f5a3
poojavarale137 <poojavarale137@gmail.com>
2021-02-23 15:39:36 +0530

    uploading 23feb into personel/pooja/python/practice

---
+++ Personel/pooja/python/practice/23feb/multiplicationtable.py
@@ -0,0 +1,7 @@
+#Program 8 : Write a Python program to create the multiplication table of a number.
+
+n = int(input("Input a number: "))
+
+
+for i in range(1,11):       # use for loop to iterate 10 times
+   print(n,'x',i,'=',n*i)
