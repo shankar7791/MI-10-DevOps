ee2b95ce3cbd1f65d137b3b70b7197445393f2a1
raju10101997 <rajendaruppulanche@gmail.com>
2021-02-24 14:52:00 +0530

    uploading 24feb practice problems

---
+++ Personel/Rajendar/Python/Practice/24feb/prac3.py
@@ -0,0 +1,13 @@
+# Assignment Operator
+# program to read a number and find the addition of the digits
+
+num = int(input("Enter a number "))
+if num < 0:
+    print("Invalid Input")
+else:
+    sum = 0
+    while num > 0:
+        digit = num % 10
+        sum += digit
+        num = num // 10
+    print("sum = ", sum)
