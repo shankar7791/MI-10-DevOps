ec2bfe12796cc21924305a826bf59b27d4d5ba68
Bunty7 <jeetpparekh@gmail.com>
2021-02-24 15:15:36 +0530

    uploading 24feb into /Personel/Jeet/Python/Practice

---
+++ Personel/Jeet/Python/Practice/24feb/arithmatic.py
@@ -0,0 +1,14 @@
+a = 4
+b = 2
+
+print('a + b:',a + b)   #addition
+
+print('a - b:',a - b)   #subtraction
+
+print('a * b:',a * b)   #multiplication
+
+print('a / b:',a / b)   #division
+
+print('a // b:',a // b) #floor division
+
+print('a ** b:',a ** b) #power
---
+++ Personel/Jeet/Python/Practice/24feb/logical.py
@@ -0,0 +1,8 @@
+a = 10
+if a>5 and a<15:
+    print('a is greater than 5 and less than 15')
+
+
+b = 4
+if b%2==0 or b>0:
+    print('it is a example of OR')
