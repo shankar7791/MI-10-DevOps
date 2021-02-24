3195ba9a89251e10fa926327fa94983d986ba8ad
kavi1410 <elankavi432@hmail.com>
2021-02-24 10:42:11 +0530

    uploading operator practice program into personal/Elankavi

---
+++ Personel/Elankavi/python/practice/feb24/logical.py
@@ -0,0 +1,14 @@
+a=int(input("ENter the first number : "))
+b=int(input("Enter the second number : "))
+if((a>b) and (b<a)):
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+if((a>b)or(a<b)):
+    print("a and b are not equal")
+else:
+    print("a and b are equal")
+if not((a>b) and (b<a)):
+    print("a is less than b")
+else:
+    print("b is less than a")
---
+++ Personel/Elankavi/python/practice/feb24/membership.py
@@ -0,0 +1,16 @@
+x = 'Hello world'
+y = {1:'a',2:'b'}
+
+
+print("1)",'H' in x)
+
+print('h' in x)
+
+
+print('Hello' not in x)
+
+
+print(1 in y)
+
+
+print('a' in y)
