e112a19aca4476fcba728cac463586fd2dde7f22
anjal189 <anjalirahane98@gmail.com>
2021-02-24 13:18:25 +0530

    uploading operator program

---
+++ Personel/Anjali/python/practice/24feb/Assignment.py
@@ -0,0 +1,27 @@
+#Assignment Operators
+num1=int(input("Enter the number"))
+num2=int(input("Enter the number"))
+
+# += operator
+num1 += num2
+print(num1)
+
+# -= operator
+num1 -= num2
+print(num1)
+
+# *= operator
+num1 *= num2
+print(num1)
+
+# /= operator
+num1 /= num2
+print(num1)
+
+# //= operator
+num1 //= num2
+print(num1)
+
+# **= operator
+num1 **= num2
+print(num1)
