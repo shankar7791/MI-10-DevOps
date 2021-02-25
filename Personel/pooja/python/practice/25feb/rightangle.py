ab7b6774c861bba54f6debb3bd64067dda82ea3a
poojavarale137 <poojavarale137@gmail.com>
2021-02-25 17:13:41 +0530

    uploading 25 feb programs

--- Personel/pooja/python/practice/25feb/rightangle.py
+++ Personel/pooja/python/practice/25feb/rightangle.py
@@ -1,15 +1,7 @@
 # Program 2 : Write a Python program to get the third side of right angled triangle from two given sides.
-def pythagoras(opposite_side,adjacent_side,hypotenuse):
-        if opposite_side == str("x"):
-            return ("Opposite = " + str(((hypotenuse**2) - (adjacent_side**2))**0.5))
-        elif adjacent_side == str("x"):
-            return ("Adjacent = " + str(((hypotenuse**2) - (opposite_side**2))**0.5))
-        elif hypotenuse == str("x"):
-            return ("Hypotenuse = " + str(((opposite_side**2) + (adjacent_side**2))**0.5))
-        else:
-            return "default value!"
-    
-print(pythagoras(3,4,'x'))
-print(pythagoras(3,'x',5))
-print(pythagoras('x',4,5))
-print(pythagoras(3,4,5))
+
+s1 = int(input('enter side 1:'))
+s2 = int(input('enter side 2:'))
+
+s3 = 180 - s1 - s2
+print(f'third side of triangle is: {s3}')
