21c392fc2198bbc833f5b6434ab76d01b486ef9b
chandu1405 <chandrakarbhagat222@gmail.com>
2021-02-25 16:58:16 +0530

    uploading python 25feb  file

---
+++ Personel/Chandrakar/Python/25feb/25febpro2.py
@@ -0,0 +1,6 @@
+#Write a Python program to get the third side of right angled triangle from two given sides.
+
+a = int(input("enter the 1st side "))
+b = int(input("enter the 1st side "))
+c = ((a**2 + b**2)**(1/2))
+print("3rd side is : ")
