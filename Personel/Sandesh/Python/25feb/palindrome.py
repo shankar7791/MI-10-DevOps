377159e9adb95d000ea2189dc05d6c7fdea955bc
Sandesh Patekar <sandeshpatekar20@gmail.com>
2021-02-25 17:28:33 +0530

    Update palindrome.py

--- Personel/Sandesh/Python/25feb/palindrome.py
+++ Personel/Sandesh/Python/25feb/palindrome.py
@@ -1,11 +1,16 @@
 num = int(input("Enter number : "))
 temp = num
-rev = 0 
+rev=0 
 while(num>0): 
     sum=num%10
     rev=rev*10+sum
     num=num//10
+    
 if(temp==rev):
     print("The number is Palindrome !!")
 else: 
-     print("The number not a palindrome !")
+    remainder = num % 10 
+    rev = (rev * 1) + remainder
+    num = num //10
+    print(f"{rev} reverse number ")
+    print("The number not a palindrome !")
