9ae3451aabafbf17826ef60cb093ef5b49cf2ff2
raju10101997 <rajendaruppulanche@gmail.com>
2021-02-25 17:35:59 +0530

    uploading 25feb practice problems

---
+++ Personel/Rajendar/Python/Practice/24feb/prac7.py
@@ -0,0 +1,7 @@
+num = 4
+i = 1
+sum1 = 1
+while(i <= num):
+    sum1 = sum1 * i
+    i = i+1
+print(sum1)
---
+++ Personel/Rajendar/Python/Practice/25feb/Addition.py
@@ -0,0 +1,3 @@
+x = int(input("Enter the no1 :"))
+y = int(input("Enter the no2 :"))
+print("Addition is :", x - (-y))
