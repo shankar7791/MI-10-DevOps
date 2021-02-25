a0406a64d7516154b22643750e4bf02b114346d5
meerhasan <meerhasanshaikh600@gmail.com>
2021-02-25 17:30:08 +0530

    oploading /Python/25Feb

--- Personel/Meerhasan/Python/25Feb/palidrome.py
+++ Personel/Meerhasan/Python/25Feb/palidrome.py
@@ -4,12 +4,15 @@
 num = int(input("Enter number : "))
 temp = num
 rev = 0
-while (num>0) :
+while (num > 0) :
     sum = num % 10
     rev = rev * 10 + sum
     num = num // 10
 if (temp == rev) :
-    print("The number is a palindrome !")
+    print(f"{rev} The number is a palindrome ! ")
 else :
-    print("The number is not a palindrome !")
+    rem = num % 10
+    rev = (rev * 1)
+    num = num // 10
+    print(f"{rev} The number is not a palindrome !")
 
