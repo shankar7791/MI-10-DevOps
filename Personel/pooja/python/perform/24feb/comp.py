7c5a878d0e0258aa8368bc1c67c41ada60370a4c
poojavarale137 <poojavarale137@gmail.com>
2021-02-25 16:00:54 +0530

    uploading the 25 feb program into pooja_work

---
+++ Personel/pooja/python/perform/24feb/comp.py
@@ -0,0 +1,34 @@
+#Arithmetic operators
+    #Assignment operators
+    # * Comparison operators
+    # Logical operators
+    #Identity operators
+    #Membership operators
+    #Bitwise operators
+    
+x=int(input("Enter the first number : "))
+y=int(input("Enter the second number : "))
+if(x==y):
+    print("x and y are equal")
+else:
+    print("x and y are not equal")
+if(x>y):
+    print("x is greater then y")
+else:
+    print("y is greater than x")
+if(x<y):
+    print("x is less than y")
+else:
+    print("y is less than y")
+if(x!=y):
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+if(x>=y):
+    print("x is greater than or equal to y")
+else:
+    print("x is not greater than or not equal to y")
+if(x<=y):
+    print("x is less than or equal to y")
+else:
+    print("x is not less than or not equal to y")
