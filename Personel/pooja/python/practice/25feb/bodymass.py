a31b6ad479eedddc9c74efb3c95f4fcdd987497e
poojavarale137 <poojavarale137@gmail.com>
2021-02-25 17:26:09 +0530

    uploding into 25 feb

--- Personel/pooja/python/practice/25feb/bodymass.py
+++ Personel/pooja/python/practice/25feb/bodymass.py
@@ -1,4 +1,5 @@
 # Program 4 : Write a Python program to calculate body mass index.
+
 height = float(input("input your height in meters :"))
 weight = float(input("input your weight in kilogram :"))
 print("your body mass index is:",round(weight / (height * height),2))
