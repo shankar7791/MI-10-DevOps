45e32e28369f7a2e852b2e32a996e6cfe469e6f8
siddiquiaatif <57148312+siddiquiaatif@users.noreply.github.com>
2021-02-26 11:06:21 +0530

    d6

--- Personel/AATIF/Python/Triangle.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Enter first side: '))
-b = int(input('Enter second side: '))
-c = int(input('Enter third side: '))
-
-s =float((a + b + c) / 2)       # calculate the semi-perimeter
-
-# calculate the area
-area = float((s*(s-a)*(s-b)*(s-c)) ** 0.5)
-print("The area of the triangle is %0.2f" %area)
