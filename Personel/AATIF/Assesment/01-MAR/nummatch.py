e58173f63cb57ee606fa4fdadc8430053f88e60a
siddiquiaatif <siddiquiaatif115@gmail.com>
2021-03-01 10:50:29 +0530

    Uploading Assesment folder of 01-Mar on branch AATIF_work

---
+++ Personel/AATIF/Assesment/01-MAR/nummatch.py
@@ -0,0 +1,11 @@
+li = numberList
+print("Given list is ", numberList)
+firstElement = numberList[0]
+lastElement = numberList[-1]
+if (firstElement == lastElement)
+return True
+else
+return False
+
+numList = [10, 20, 30, 40, 10]
+print("result is", li(numList))
