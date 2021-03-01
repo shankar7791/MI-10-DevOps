1fdf60fc29f3f944427a20e2201591baea1f8271
siddiquiaatif <siddiquiaatif115@gmail.com>
2021-03-01 17:01:50 +0530

    Uploading practice folder of 01-Mar on branch AATIF_work

---
+++ Personel/AATIF/Python/Practice/01-MAR/Conversn.py
@@ -0,0 +1,10 @@
+x=0
+while x<5:
+     x=x+1
+     print ("Output of while loop statement: ",x)
+
+print("_______________________________________________")
+
+
+for x in range(5):
+     print ("Output of for loop statemant: ",x)
---
+++ Personel/AATIF/Python/Practice/01-MAR/range.py
@@ -0,0 +1,4 @@
+for x in range(9):
+  print(x)
+else:
+  print("Finally finished!")
