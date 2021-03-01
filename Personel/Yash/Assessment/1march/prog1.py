21fb4f93397a1edfdc39b94b28b787038ccf80f8
coolyasu <malavade47@gmail.com>
2021-03-01 10:21:27 +0530

    Uploading Tasks Assessment programs in Assessment/1march folder

---
+++ Personel/Yash/Assessment/1march/prog1.py
@@ -0,0 +1,12 @@
+# Program 1 : Print the following pattern using while loop 
+
+num=int(input("Enter the no of rows : "))
+
+i=num
+while i>=1:
+    j=i
+    while j>= 1:
+        print(j,end=" ")
+        j=j-1
+    print()
+    i=i-1
