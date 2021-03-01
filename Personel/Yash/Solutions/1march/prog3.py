56697786c50b9979ff510e5556affdefb3c8c0b8
coolyasu <malavade47@gmail.com>
2021-03-01 11:54:13 +0530

    Uploading Assessment Solutions in Solutions/1march folder

--- Personel/Yash/Solutions/1march/prog3.py
+++ Personel/Yash/Solutions/1march/prog3.py
@@ -1,15 +1,15 @@
 # Program 3 : Given a list of numbers, return True if first and last number of a list is same
 
-lst = ["yash",1,2,3,True,"abc","yash"]
-if lst[0] == lst[len(lst)-1]:
-    print(True)
-else:
-    print(False) 
-
-# n=input("Enter a list of numbers :")
-# l=list(n)
-# print("The original list is :",l)
-# if l[0] == l[-1]:
+# lst = ["yash",1,2,3,True,"abc","yash"]
+# if lst[0] == lst[len(lst)-1]:
 #     print(True)
 # else:
-#     print(False)
+#     print(False) 
+
+n=input("Enter a list of numbers :")
+l=list(n)
+print("The original list is :",l)
+if l[0] == l[-1]:
+    print(True)
+else:
+    print(False)
