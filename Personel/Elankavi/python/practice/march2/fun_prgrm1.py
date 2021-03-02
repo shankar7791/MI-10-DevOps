77e94e50346ce80f364e6bb915acafa3f1b54adf
kavi1410 <elankavi432@hmail.com>
2021-03-02 16:42:34 +0530

    uploading practice problem into personal/Elankavi

---
+++ Personel/Elankavi/python/practice/march2/fun_prgrm1.py
@@ -0,0 +1,10 @@
+def fun():
+    a=int(input("Enter the number : "))
+    if (a % 2 == 0):
+        print("Even number")
+    else:
+        print("odd number")
+i=1
+while(i<=10):
+    fun()
+    i+=1
---
+++ Personel/Elankavi/python/practice/march2/fun_prgrm2.py
@@ -0,0 +1,8 @@
+i=1
+a=input("enter the character : ")
+def fun():
+    print(a)
+while(i<=3):
+    fun()
+    i+=1
+
