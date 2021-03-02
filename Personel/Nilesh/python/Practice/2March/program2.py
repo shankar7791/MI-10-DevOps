da5e645b5ecaf1b0053cd2226afa83f2c13dc99e
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-03-02 14:31:55 +0530

    uploading function practuce programs nilesh/python/practice/2march

---
+++ Personel/Nilesh/python/Practice/2March/program2.py
@@ -0,0 +1,10 @@
+#simple addition program using function.
+def sum():
+    A = int(input("enter the number : "))
+    B = int(input("enter the number : "))
+
+    sum = A + B
+
+    print("Sum of A and B is : ",sum)
+
+sum()
---
+++ Personel/Nilesh/python/Practice/2March/program3.py
@@ -0,0 +1,10 @@
+def checkNum():
+
+    num = int(input("Enter the number: " ))
+
+    if num >= 0:
+        print("Positive or zero ")
+    else:
+        print("Negative number")
+
+checkNum()
