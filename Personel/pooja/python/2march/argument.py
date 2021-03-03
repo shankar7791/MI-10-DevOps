b9b732da18cd6a0dc3d0b2de598b77e1599940c1
poojavarale137 <poojavarale137@gmail.com>
2021-03-03 09:29:18 +0530

    uploading programs 2march/personel/pooja/python

---
+++ Personel/pooja/python/2march/argument.py
@@ -0,0 +1,7 @@
+def func(fname):
+    print(fname + " pooja")
+
+
+func("email")
+func("address")
+func("linux")
---
+++ Personel/pooja/python/2march/listargu.py
@@ -0,0 +1,7 @@
+def func(food):
+    for x in food:
+        print(x)
+
+
+fruit = ["apple", "banana", "cherry"]
+func(fruit)
