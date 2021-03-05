3900aea5bcf9c51f171b1b19546cf581584173c5
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-03-05 18:34:11 +0530

    uploading programs/algo and flowchart nilesh/python

---
+++ Personel/Nilesh/python/5March/program4/program4.py
@@ -0,0 +1,17 @@
+#Python program to find the sum of sequence of all numbers upto the given number using recursive function.
+
+#step-1: take a input from user 
+#step-2: define the function as recur_sum.
+#step-3: check if n is less than or equal to 1 then return n.
+#step-4: otherwise go to else part and return n + and call recursive method(n-1).
+#step-5: call the function and print the result.
+
+ 
+n = int(input("enter the number : "))
+def recur_sum(n):
+   if n <= 1:
+       return n
+   else:
+       return n + recur_sum(n-1)
+
+print(recur_sum(n))
