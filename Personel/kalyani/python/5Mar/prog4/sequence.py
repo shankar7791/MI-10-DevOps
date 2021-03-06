31971195928d95a32ddfbe1dc0655bc1cca2d9b2
KalyaniArunburde <71255579+KalyaniArunburde@users.noreply.github.com>
2021-03-06 15:52:51 +0530

    Add files via upload

--- Personel/kalyani/python/5Mar/prog4/sequence.py
+++ Personel/kalyani/python/5Mar/prog4/sequence.py
@@ -1,2 +1,24 @@
 
 # WAP TO sum of sequence of all nos upto the given no using recursion function.
+
+def recur_sum(n):
+   if n <= 1:
+       return n
+   else:
+       return n + recur_sum(n-1)
+
+
+num = 25 # take input from user 
+   print("Enter a positive number")
+else:
+   print("The sum is",recur_sum(num))
+   
+   # Alogrithm
+   # create sum function
+   # conditional statemnet
+   # print statment
+   # else statment 
+   # take input from user
+   # print positive no
+   # else statment
+   # print recur sum no
