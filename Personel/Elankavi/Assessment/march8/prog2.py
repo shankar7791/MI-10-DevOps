302e03ab0b3502df968bb190937d31112c8f700d
kavi1410 <elankavi432@hmail.com>
2021-03-08 14:37:50 +0530

    Uploading assessment into Personal/Elankavi

---
+++ Personel/Elankavi/Assessment/march8/prog2.py
@@ -0,0 +1,12 @@
+# program to check the binary representation is pallindrome or not
+
+n=int(input('Enter a number: '))
+ 
+b=int(bin(n)[2:])
+rev=str(b)[::-1] 
+if int(rev)==b:
+    print(f"Binary representation of n={n} is {b} which is pallindrome ")
+else:
+    print(f"Binary representation of n={n} is {b} which is not a pallindrome ")
+
+
