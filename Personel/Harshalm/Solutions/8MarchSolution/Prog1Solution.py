8a87ddb8dcfcd7292335e27334183b37c0509488
harshlmeshram <hmeshram557@gmail.com>
2021-03-08 17:53:07 +0530

    Uploading 8-March Assessment Solutions

---
+++ Personel/Harshalm/Solutions/8MarchSolution/Prog1Solution.py
@@ -0,0 +1,14 @@
+
+#Python program to print alphabet pattern 'A'
+
+
+n = int(input("Enter the row : "))
+
+for i in range(n):
+    for j in range((n // 2)+1):
+        if ((j == 0 or j == n // 2) and i != 0 or i == 0 and j != 0 and j != n // 2 or i == n // 2): 
+                print("*", end = "") 
+        else: 
+                print(" ", end = "") 
+
+    print() 
