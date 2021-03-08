68584a3673feeacf3222d0ce7f0191c75651941b
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-03-08 14:02:30 +0530

    uploading assesment nilesh/python/assesment/8march

---
+++ Personel/Nilesh/python/Assesment/8March/program1.py
@@ -0,0 +1,18 @@
+#write a python program to print pattern of 'A'.
+
+for row in range(0,3):
+    print(" *",end="")
+print("\n")
+print("*    *",end="")
+print("\n")
+print("*    *",end="")
+print("\n")
+for row in range(0,5):
+    print("*",end="")
+print("\n")
+print("*    *",end="")
+print("\n")
+print("*    *",end="")
+print("\n")
+print("*    *",end="")
+
