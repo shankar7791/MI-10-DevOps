558457c1036c7d501c496727ddd9f52f460ae03f
sandeshpatekar <sandeshpatekar20@gmail.com>
2021-03-08 15:17:12 +0530

    remaining programs

--- Personel/Sandesh/Python/Practice/1march/2.for_loop..py
+++ Personel/Sandesh/Python/Practice/1march/2.for_loop..py
@@ -2,6 +2,5 @@
 
 Shopping_websites = ["Amazon", "Flipkart", "Myntra"]
 
-# iterate over the list using index
 for i in range(len(Shopping_websites)):
 	print("I like do shopping on", Shopping_websites[i])
