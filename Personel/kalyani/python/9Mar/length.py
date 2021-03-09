260ec58226ec0c329bd16dd99ec9fef22fd8ef87
kalyaniarunBurde <kalyaniburde1995@gmail.com>
2021-03-09 15:52:53 +0530

    uploading string program 9Mar into python/kalyani

---
+++ Personel/kalyani/python/9Mar/length.py
@@ -0,0 +1,8 @@
+# WAP TO calculate length of string using function
+
+def string_length(str1)
+count = 0
+for char in str 1:
+    count + =1 
+    return count
+print(string_length('kalyani'))
---
+++ Personel/kalyani/python/9Mar/palindrone.py
@@ -0,0 +1,10 @@
+
+# WAP TO check a string is palindrone or not 
+
+a = int(input("Enter string :"))
+
+b = a[-1 ::-1]
+if ( a == b)
+print ( " Palindrone string ")
+else:
+    print ( " Not Palindrone string ")
