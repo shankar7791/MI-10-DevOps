1648be98a8a355552d6e3f2a4e84d9b301afb69e
varmarajkumar745 <varmarajkumar745@gmail.com>
2021-03-09 14:52:07 +0530

    uploding practice string program Personal/Rajkumar/Python/practice/9-march

---
+++ Personel/Rajkumar/Python/practice/9-march/str9.py
@@ -0,0 +1,9 @@
+#Escaping 
+# using triple quotes  
+print('''''They said, "What's there?"''')  
+  
+# escaping single quotes  
+print('They said, "What\'s going on?"')  
+  
+# escaping double quotes  
+print("They said, \"What's going on?\"")  
