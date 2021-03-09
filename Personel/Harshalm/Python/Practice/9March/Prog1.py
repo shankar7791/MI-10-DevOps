ac7916baaf071b70fb61e8a86d15696ca9016fd1
harshlmeshram <hmeshram557@gmail.com>
2021-03-09 20:35:25 +0530

    Uploading 9-March Practice Program

---
+++ Personel/Harshalm/Python/Practice/9March/Prog1.py
@@ -0,0 +1,26 @@
+str = "Software Engineering"
+print(str)        #assign string variable
+
+
+str1 = """Hey, how are you?"""
+print(str1)       #multiline string
+
+
+str2 = "college names"
+print(str[2])      #string array
+
+for i in "College" :
+    print(i)    #looping string
+
+str3 = "College Names"
+print(len(str3))
+
+str4 = "Orange city is Nagpur"
+print("Nagpur" in str4)    #check string
+
+str5 = "Freedom is the best life ever"
+if "life" in str5 :
+    print("yes, 'life' is present ")    #if-string
+
+str6 = "The best line  is poetry"
+print("life" not in str6)  #not check
