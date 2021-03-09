5cc58de259ed671f7dcfb44bb0ae5393606ce953
kavi1410 <elankavi432@hmail.com>
2021-03-09 16:24:22 +0530

    uploading practice problem into personal/Elankavi

---
+++ Personel/Elankavi/python/practice/march9/prog3.py
@@ -0,0 +1,6 @@
+def join():
+    a=input("Enter your first name : ")
+    b=input("Enter your second name : ")
+    c=a+b
+    print("Your name is : ",c)
+join()
---
+++ Personel/Elankavi/python/practice/march9/prog4.py
@@ -0,0 +1,16 @@
+# Escape character
+
+a=input("Enter your name : ")
+print(f"your name is \"{a}\" ")
+# to print the new line 
+print(f"your name is \n \"{a}\" ")
+#in single quotes
+print(f"your name is \'{a}\' ")
+#backspace 
+print(f"your name is  \b{a}\b ")
+#form feed
+print(f"your name is \f{a}\f ")
+#carrige return
+print(f"your name is \r{a}\r ")
+#Tab
+print(f"your name is \t{a}\t ")
