52c6b693bee6ed5a29630e25155c72e54af80ffc
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-03-12 14:48:42 +0530

    modified program1.py

---
+++ Personel/Nilesh/python/Practice/12March/program1.py
@@ -0,0 +1,23 @@
+# Program to accept the strings which contains all vowels
+
+def check(string) :
+ 
+    string = string.lower()
+
+    vowels = set("aeiou")
+ 
+    s = set({})
+ 
+    for char in string :
+ 
+        if char in vowels :
+            s.add(char)
+        else:
+            pass
+    if len(s) == len(vowels) :
+        print("Accepted")
+    else :
+        print("Not Accepted")
+
+string = input("Enter the string : ")
+check(string)
