d11a173dcc59c37404799ea962f9b6fbffaa9a12
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-03-12 14:43:02 +0530

    uploading 12March task programs

---
+++ Personel/Nilesh/python/Practice/12March/program7.py
@@ -0,0 +1,8 @@
+#Remove empty tuples from a list
+def Remove(tuples): 
+    tuples = [t for t in tuples if t] 
+    return tuples 
+
+tuples = [(), ('ram','15','8'), (), ('laxman', 'sita'),  
+          ('krishna', 'akbar', '45'), ('',''),()] 
+print(Remove(tuples)) 
