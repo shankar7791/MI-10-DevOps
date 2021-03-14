73175a32c98318cdc74ac3d94c5089473f3d5642
kalyaniarunBurde <kalyaniburde1995@gmail.com>
2021-03-14 12:09:24 +0530

    uploading programs

---
+++ Personel/kalyani/python/10Mar/singlestr.py
@@ -0,0 +1,4 @@
+
+# WAP to get a single string from two given strings, separated by a space and swap the first two characters of each string.Sample String : 'abc', 'xyz'
+Expected Result : 'xyc abz' (edited) 
+
---
+++ Personel/kalyani/python/12Mar/reversing.py
@@ -0,0 +1,9 @@
+#Reversing a tuple
+
+a=tuple(input("Enter the character : "))
+
+print("Your character : ",a)
+
+s=a[::-1]
+
+print("Your reverse character : ",s)
