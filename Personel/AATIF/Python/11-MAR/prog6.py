23ec31491f62832c69cecc5ac8027074fad82ef9
siddiquiaatif <siddiquiaatif115@gmail.com>
2021-03-15 00:40:15 +0530

    Uploading the 11-MAR assignment folder on branch AATIF_work

---
+++ Personel/AATIF/Python/11-MAR/prog6.py
@@ -0,0 +1,13 @@
+# Convert a list of characters into a string
+# Input : ['p', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g']
+# Output : programming
+
+def convert(s): 
+    new = "" 
+
+    for x in s: 
+        new += x    
+    return new 
+      
+s = ['P','r','o','g', 'r','a','m','m','i','n','g'] 
+print(convert(s)) 
