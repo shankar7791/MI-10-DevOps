6252ce8fc796f4018b5aa35b402565567355aed0
avdhoot13 <mane.avdhoot13@gmail.com>
2021-03-16 12:15:07 +0530

    Assessment Mar 16

---
+++ Personel/Avdhoot/Assessment/16-Mar/prog1.py
@@ -0,0 +1,9 @@
+ls = [12, 35, 9, 56, 24]
+print("List before Interchange = ", ls)
+ls[0], ls[4] = ls[4], ls[0]
+print("List after Interchange = ", ls)
+
+ls1 = [1,2,3]
+print("List before Interchange = ", ls1)
+ls1[0], ls1[2] = ls1[2], ls1[0]
+print("List after Interchange = ", ls1)
---
+++ Personel/Avdhoot/Assessment/16-Mar/prog5.py
@@ -0,0 +1,14 @@
+def sum(dict, dict2):
+    sum1 = 0
+    sum2 = 0
+    for i in dict :
+        sum1 += dict[i]
+    print(sum1)
+    
+    for j in dict2 :
+        sum2 += dict2[j]
+    print(sum2)
+
+dict = {'a': 100, 'b':200, 'c':300}
+dict2 = {'x': 25, 'y': 18, 'z': 45}
+sum(dict, dict2)
