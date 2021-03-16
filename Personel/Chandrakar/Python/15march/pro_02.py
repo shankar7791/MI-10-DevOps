4891a9bef58f60c14181ad69a2c2a954f6e7db48
chandu1405 <chandrakarbhagat222@gmail.com>
2021-03-16 15:28:46 +0530

    uploading python remain task  file

---
+++ Personel/Chandrakar/Python/15march/pro_02.py
@@ -0,0 +1,14 @@
+def Anagram():
+    w=('cat', 'dog', 'tac', 'god', 'act')
+    dict={}
+    for word in w:
+        sortedword="".join(sorted(word))
+        if (sortedword not in dict):
+            dict[sortedword]=[word]
+        else:
+            dict[sortedword].append(word)
+        result=[]
+        for item in dict.values():
+            result.append(item)
+    print(result)
+Anagram() 
