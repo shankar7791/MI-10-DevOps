354fec5d6b00fa92eaab92a70d585bef7b907269
kalyaniarunBurde <kalyaniburde1995@gmail.com>
2021-03-16 12:26:19 +0530

    uploading program of 16MAr into Assignment/kalyani

---
+++ Personel/kalyani/Assignment/16Mar/prog4.py
@@ -0,0 +1,6 @@
+# Remove all duplicates from a given string in python.
+
+from collection import orderedDict
+def_remove_duplicates(Str1):
+    return "" . join(orderedDict.fromkeys(Str1)):
+print(remove_duplicates(" geeksforgeeks"))
