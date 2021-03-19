098d588b277c645a044cd58951c08d17271e1f88
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-03-19 13:04:49 +0530

    uploading the practice array programs

---
+++ Personel/Nilesh/python/Practice/19March/program1.py
@@ -0,0 +1,9 @@
+#Array
+
+arr = [1,2,3,4,5,6,7,8,9]
+print(arr)
+for i in arr:
+    print(i)
+print("Max element in the array is : ",max(arr))
+print("min element in th array is : ",min(arr))
+print("length of the array is  : ",len(arr))
