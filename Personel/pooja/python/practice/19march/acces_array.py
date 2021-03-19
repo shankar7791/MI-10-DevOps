5608dc49fdb592a781b8e3b405f61b80ba467f1c
poojavarale137 <poojavarale137@gmail.com>
2021-03-19 12:39:23 +0530

    uploading programs in practice

---
+++ Personel/pooja/python/practice/19march/acces_array.py
@@ -0,0 +1,5 @@
+import array as arr
+a = arr.array('i', [1, 2, 3, 4, 5, 6])
+print("first element : ", a[0])
+print("second element:", a[2])
+print("last elemnt:", a[-1])
---
+++ Personel/pooja/python/practice/19march/extend_array.py
@@ -0,0 +1,4 @@
+import array as arr
+num = arr.array('i', [1, 2, 3, ])
+num.extend([4, 5, 6, 7])
+print(num)
