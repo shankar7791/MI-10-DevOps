1287936d94a7f8f67a3c2c3abd665c5f1d3e1f3f
raju10101997 <rajendaruppulanche@gmail.com>
2021-03-23 21:31:30 +0530

    uploading 23mar

---
+++ Personel/Rajendar/Python/Practice/23mar/app.py
@@ -0,0 +1,8 @@
+# Program 1 : Write a Python class to reverse a string word by word.
+
+class py_solution:
+    def reverse_words(self, s):
+        return ' '.join(reversed(s.split()))
+
+
+print(py_solution().reverse_words('hello .py'))
---
+++ Personel/Rajendar/Python/Practice/23mar/app3.py
@@ -0,0 +1,10 @@
+class py_solution:
+    def twoSum(self, nums, target):
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return (lookup[target - num], i)
+            lookup[num] = i
+
+
+print(py_solution().twoSum((10, 20, 10, 40, 50, 60, 70), 50))
