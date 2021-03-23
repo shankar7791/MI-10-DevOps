bfd11aea896beb2ee2470b780379c44e89b1cdbb
poojavarale137 <poojavarale137@gmail.com>
2021-03-23 22:30:16 +0530

    uploding class programs

---
+++ Personel/pooja/python/class/variable.py
@@ -0,0 +1,19 @@
+# variables with a value
+class CSStudent:
+
+    stream = 'cse'
+
+    def __init__(self, roll):
+
+        self.roll = roll
+
+
+a = CSStudent(101)
+b = CSStudent(102)
+
+print(a.stream)
+print(b.stream)
+print(a.roll)
+
+
+print(CSStudent.stream)
