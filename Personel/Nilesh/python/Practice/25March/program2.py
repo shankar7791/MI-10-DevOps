ebc53181a7adb1c846bdf0e08095433a52d31790
NilPatil7087 <nilesh.patil9256@gmail.com>
2021-03-25 16:06:10 +0530

    uploading 25 March programs

---
+++ Personel/Nilesh/python/Practice/25March/program2.py
@@ -0,0 +1,19 @@
+#single inheritance
+class ParentClass:
+
+    def feature_1(self):
+        print('feature_1 from ParentClass is running...')
+
+    def feature_2(self):
+        print('feature_2 from ParentClass is running...')
+
+
+class ChildClass(ParentClass):
+
+    def feature_3(self):
+        print('feature_3 from ChildClass is running...')
+
+obj = ChildClass()
+obj.feature_1()
+obj.feature_2()
+obj.feature_3()
