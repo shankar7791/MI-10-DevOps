988b2134f30af4aab4b2fefa966e8ae5e6439580
kavi1410 <elankavi432@hmail.com>
2021-05-19 21:19:20 +0530

    uploading filehandling prog

---
+++ Personel/Elankavi/python/filehandling/prog1.py
@@ -0,0 +1,18 @@
+ar=list(input("Enter the list : "))
+f=open("file1.txt","w+")
+print("Before Sorting")
+for i in ar:
+    print(i,end=" ")
+    f.write(str(i))
+f.close()
+f1=open("file1.txt","r+")
+d=f1.read()
+print("\n After sorting")
+data=sorted(d)
+for i in data:
+    print(i,end=" ")
+
+f1.close()
+    
+
+
