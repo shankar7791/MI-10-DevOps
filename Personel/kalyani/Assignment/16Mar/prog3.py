a781c4894f4dbc08fcebad6e0822ecfb95c4e0cd
KalyaniArunburde <71255579+KalyaniArunburde@users.noreply.github.com>
2021-05-23 14:01:03 +0530

    Revert "uploading program of 16MAr into Assignment/kalyani"

--- Personel/kalyani/Assignment/16Mar/prog3.py
+++
@@ -1,9 +0,0 @@
-# check if a substring is present in a given string
-
-
-string = int(input(" Enter string :"))
-sub_string = int(input("Enter word : "))
-if ( string.find( sub_string) == -1):
-    print(" Substring not found in string :")
-else:
-    print(" Substring in string :")
--- Personel/kalyani/Assignment/16Mar/prog4.py
+++
@@ -1,6 +0,0 @@
-# Remove all duplicates from a given string in python.
-
-from collection import orderedDict
-def_remove_duplicates(Str1):
-    return "" . join(orderedDict.fromkeys(Str1)):
-print(remove_duplicates(" geeksforgeeks"))
