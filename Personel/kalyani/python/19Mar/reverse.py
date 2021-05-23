67aa61dd878cf8ee937632c416ba120b60992f9d
KalyaniArunburde <71255579+KalyaniArunburde@users.noreply.github.com>
2021-05-23 13:47:33 +0530

    Revert "Revert "uploading array program into python/kalyani""

---
+++ Personel/kalyani/python/19Mar/reverse.py
@@ -0,0 +1,14 @@
+# Python program to print element of an array in reverse order.
+
+arr = [ 1,2,3,4,5]
+
+print(" original array:")
+
+for i in range ( 0,len(arr)):
+    print(arr[i])
+        
+print(" Array in reverse order :")
+
+for i in range(len(arr) -1,-1,-1)
+
+print(arr[i])
