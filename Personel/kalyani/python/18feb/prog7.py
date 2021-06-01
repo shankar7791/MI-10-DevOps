0ac897b3914d863cc2b770fe0053bc63fae40ef2
kalyaniarunBurde <kalyaniburde1995@gmail.com>
2021-06-01 12:24:09 +0530

    uploading programs into python/kalyani

---
+++ Personel/kalyani/python/18feb/prog7.py
@@ -0,0 +1,9 @@
+#Program to input any alphabets and check it is vowel or consonant
+
+ch = input("Enter an alphabet :")
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print(ch, "is a Vowel")
+else:
+    print(ch, "is a Consonant")
