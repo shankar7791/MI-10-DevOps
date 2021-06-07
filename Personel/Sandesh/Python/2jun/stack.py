871b2e6b53b567d25766c0ecccf2dab8f4c62371
Sandesh Patekar <sandeshpatekar20@gmail.com>
2021-06-07 20:22:10 +0530

    stack and Queue

---
+++ Personel/Sandesh/Python/2jun/stack.py
@@ -0,0 +1,23 @@
+# stack LIFO
+
+
+stack = []
+
+
+
+stack.append("Sandesh")
+stack.append("Patekar")
+stack.append("MI-10")
+
+
+print("Elements Inserted In Stack  : ", stack)
+
+
+print("\nPoped Element From Stack : ", stack.pop())
+print("Stack : ", stack)
+
+print("\nPoped Element From Stack : ", stack.pop())
+print("Stack : ", stack)
+
+print("\nPoped Element From Stack : ", stack.pop())
+print("Stack : ", stack)
---
+++ Personel/Sandesh/Python/Searching_Alogithms/Binary_Search.py
@@ -0,0 +1,16 @@
+from bisect import bisect_left
+
+def BinarySearch(a_list, value):
+	i = bisect_left(a_list, value)
+	if i != len(a_list) and a_list[i] == value:
+		return i
+	else:
+		return -1
+
+a_list = [1, 2, 4, 4, 8]
+value = int(input("Enter Number To Search : "))
+ob = BinarySearch(a_list, value)
+if ob == -1:
+	print(f"{value} Not Found !!")
+else:
+	print(f"{value} Is Present At Index {ob}")
