68a591322f872a93ef55aa8f3b6192b242efde4b
kalyaniarunBurde <kalyaniburde1995@gmail.com>
2021-06-16 14:37:29 +0530

    uploading programs of stack and deque

---
+++ Personel/kalyani/python/2June/stack/prog1.py
@@ -0,0 +1,20 @@
+# Write a program to implement stack using insert and remove element.
+
+s=[]
+
+print("inserting elements")
+
+s.insert(1,"java")
+s.insert(2,"python")
+s.insert(3,"c++")
+
+print(s)
+
+print("removing elements")
+
+s.pop()
+
+print(" removed ")
+print(s)
+
+s.pop()
