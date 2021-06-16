e039721789924c9d113efcd0f9d19e877b6d4d37
kalyaniarunBurde <kalyaniburde1995@gmail.com>
2021-06-16 14:34:19 +0530

    uploading programs of binary search tree

---
+++ Personel/kalyani/python/7June/binarysearch.py
@@ -0,0 +1,35 @@
+# construct binary serach tree and perform insert delete and search operations.
+
+class BinaryTree:
+    def__init__(self,data):
+        self.data = data 
+        self.right = None
+        self.left = None
+        
+    def__insert(self,data) :
+        if self.key is None :
+           self.key=data 
+           return: 
+        if self.key>data 
+           if self.Left:
+               self.left.insert(data)
+           else:
+               self.left=BinaryTree(data)
+        else:
+            if self.right:
+                   self.right.insert(data)
+           else:
+               self.right=BinaryTree(data)
+               
+    def__delete(self,data):
+                
+            
+    
+root = BinaryTree(10)
+print(root.data)
+print(root.left)
+print(root.right)
+list 1=[20,1,2,3]
+for i in List 1
+    root.insert(i)
+    
